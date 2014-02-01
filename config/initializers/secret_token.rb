ea7ad2d7ab390b43f5732cbb7762bdb6b089cc63
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2014-02-01 19:03:36 +0400

    Initial: scaffolded

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shipper::Application.config.secret_key_base = '259ca4651cc70a5b6fc3799adf470368140bb4c3b2ea7e3d6146a5c78d4d9f5f583a1750a2b473ae770c5bcfd37d4436b035ccf16163981684aae7f6b60c73a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shipper::Application.config.session_store :cookie_store, key: '_shipper_session'
