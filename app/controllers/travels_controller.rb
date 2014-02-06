a681cab4a4187adc0544b3ed335c62aac60dca6a
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2014-02-06 11:11:35 +0400

    Continue travels-harness shipping: allow taking travels as a performer, allow listing travels

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,11 +1,24 @@
 class TravelsController < ApplicationController
 
+  require_login :show, :index, :new, :create, :take
+
+  # GET /travels/:id
   def show
     @travel = Travel.find(whitelist(params, :show))
 
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @travel, status: 200 }
+    end
+  end
+
+  # GET /travels
+  def index
+    @travels = Travel.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @travels, status: 200 }
     end
   end
 
@@ -27,7 +40,26 @@ class TravelsController < ApplicationController
         format.html { redirect_to travel_path(@travel), notice: 'Gracefully created!' }
         format.json { render json: @travel, status: :created, location: @travel }
       else
-        raise "Fuck you!"
+        format.html { render action: :new }
+        format.json { render json: @travel.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # POST /travels/:id/take
+  def take
+    sanitized = whitelist(params, :take)
+
+    travel    = Travel.find(sanitized[:id])
+    performer = Users::Performer.find(sanitized[:performer])
+
+    travel.performer = performer
+
+    respond_to do |format|
+      if travel.save
+        format.json { render json: @travel, status: :ok, location: @travel }
+      else
+        raise
       end
     end
   end
@@ -46,6 +78,13 @@ class TravelsController < ApplicationController
                   :destination,
                   { items_attributes: [ :name, :description, :weight ] }
                 )
+
+        when :take
+          {
+            id:         params.require(:id),
+            performer:  params.require(:performer)
+          }
+
         else
           raise "Couldn't whitelist unknown action!"
       end
