ace251ba208f3e5f8845b9a4478e656819242036
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2014-03-09 21:11:48 +0400

    Travels creation form re-styled; form javascript harness revisited

--- app/assets/javascripts/application_helper.js
+++ app/assets/javascripts/application_helper.js
@@ -1,6 +1,7 @@
 
-function add_fields(self, association, content) {
-    var nid = new Date().getTime();
-    var re  = new RegExp("new_" + association, 'g');
-    $(self).before(content.replace(re, nid));
+function add_fields_before(anchor, content) {
+    if ($.isFunction(content))
+        $(anchor).before(content.apply());
+    else
+        $(anchor).before(content);
 }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,13 +4,23 @@ module ApplicationHelper
 
   module ViewHelper
 
-    def add_link_to_fields(form, association)
+    def add_link_to_fields(form, anchor, association)
       singular  = association.to_s.singularize.sub(/[\[\]]/, '')
       instance  = form.object.class.reflect_on_association(association).klass.new
       content   = form.fields_for(association, instance, :child_index => 'new_' + singular) do |subform|
         render singular + '_field', :form => subform
       end
-      link_to_function('Add ' + association.to_s, "add_fields(this, \"#{singular}\", \"#{escape_javascript(content)}\")")
+
+      content = "function() {
+                  var nid = new Date().getTime();
+                  var re  = new RegExp(\"#{"new_" + singular}\", 'g');
+
+                  return \"#{escape_javascript(content)}\".replace(re, nid);
+                }"
+
+      onclick = "add_fields_before(#{anchor}, #{content})"
+
+      button_to_function('Add ' + association.to_s, onclick, class: "btn btn-primary")
     end
 
   end
