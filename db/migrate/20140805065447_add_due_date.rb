80495a8ee5b3d8c7ad8ae4fa5f3ca78f0030f07a
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2014-08-12 11:02:11 +0400

    Hack, to overcome stupidity of patching existing migrations

--- db/migrate/20140805065447_add_due_date.rb
+++ db/migrate/20140805065447_add_due_date.rb
@@ -1,5 +1,7 @@
 class AddDueDate < ActiveRecord::Migration
   def change
+    drop_table :due_dates if table_exists? :due_dates
+
     create_table :due_dates do |t|
       t.datetime :starts,   null: false
       t.datetime :ends,     null: false
